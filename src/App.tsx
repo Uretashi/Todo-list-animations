c944a7dba567e7f9f825a4940f9cd9f68e482517
Kahbei <kabe.61st@gmail.com>
2022-04-05 14:34:31 +0200

    Enhance the header

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,24 @@ function App() {
   return (
     <>
         <header className="">
-          <span>
-            <Link to="/">Accueil</Link>
-          </span>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+              <Link to="/" className="navbar-brand">Home</Link>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link to="/" className="nav-link active">Home</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/" className="nav-link">Link</Link>
+                  </li>               
+                </ul>
+              </div>
+            </div>
+          </nav>
         </header>
 
       <Routes>
