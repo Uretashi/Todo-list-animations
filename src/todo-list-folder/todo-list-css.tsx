9fc755f6c220486a90849af93f5a8051976efc92
Kahbei <kabe.61st@gmail.com>
2022-04-07 16:32:49 +0200

    Refactor from class to function

--- src/todo-list-folder/todo-list-css.tsx
+++ src/todo-list-folder/todo-list-css.tsx
@@ -1,91 +1,97 @@
-import React from 'react';
-import style from './todo-list-css.module.css';
+import React, { useState } from "react";
+import style from "./todo-list-css.module.css";
 
+const TodoListForm = (props: any) => {
+    return (
+        <div className={style.todoMainDiv}>
+            <form className={style.addTodoForm} onSubmit={props.handleSubmit}>
+                <label htmlFor="todoName">Todo : </label>
+                <input
+                    className={style.addTodoInput}
+                    placeholder="Add your task"
+                    name="todoName"
+                    type="text"
+                    onChange={props.handleChange}
+                />
+                <input className={style.addTodoBtn} value={"Ajouter"} type="submit" />
+            </form>
+        </div>
+    );
+};
 
-export default class TodoListCss extends React.Component<{}, { addTodo: string, todoList: Array<string> }> {
-    test: number = 1;
-
-    constructor(props: any) {
-        super(props);
-        this.state = { addTodo: '', todoList: ['Buy groceries', 'Go for a ice cream', 'Find some cool memes'] };
-
-        // lie les fonctions au composant
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+function TodoListCss() {
+    const [addTodo, setAddTodo] = useState("");
+    const [todoList, setTodoList] = useState([
+        "Buy groceries",
+        "Go for a ice cream",
+        "Find some cool memes",
+    ]);
 
     /**
      * Supprime un élément du tableau todoList
-     * 
+     *
      * @param index élement à supprimé
      * @returns void
      */
-    deleteTodoElement(index: number, event:any): void {
-        // this.test = 0
-        event.target.style.transform = 'translateX(180px)'
-        event.target.parentNode.style.opacity = 0
+    const deleteTodoElement = (index: number, event: any): void => {
+        event.target.style.transform = "translateX(180px)";
+        event.target.parentNode.style.opacity = 0;
 
         setTimeout(() => {
-            this.state.todoList.splice(index, 1);
-            this.setState({});
+            todoList.splice(index, 1);
+            setTodoList([...todoList]);
         }, 500);
-        
-    }
+    };
 
     /**
      * Modifie la valeur de addTodo pour préparer cette dernière à être ajoutée à la liste
-     * 
+     *
      * @param event événement ayant invoqué la fonction (input)
      * @returns void
      */
-    handleChange(event: any): void {
-        this.setState({ addTodo: event.target.value, todoList: this.state.todoList });
-    }
+    const handleChange = (event: any): void => {
+        setAddTodo(event.target.value);
+        setTodoList(todoList);
+    };
 
     /**
      * Ajoute la valeur de addTodo à la todoList
-     * 
+     *
      * @param event événement ayant invoqué la fonction (input submit)
      * @returns void
      */
-    handleSubmit(event: any): void {
+    const handleSubmit = (event: any): void => {
         // reset la valeur de l'input
         event.target.reset();
 
-        if(this.state.addTodo !== '') {
-            this.setState({ addTodo: '', todoList: [...this.state.todoList, this.state.addTodo] });
+        if (addTodo !== "") {
+            setTodoList([...todoList, addTodo]);
+            setAddTodo("");
         }
 
         event.preventDefault();
 
-        
         setTimeout(() => {
-            const newTaskAnimation = document.querySelector('.'+style.todoObject+":last-child");
-            newTaskAnimation?.classList.add(style.newTaskAnimation)
-        }, 1)
-    }
+            const newTaskAnimation = document.querySelector("." + style.todoObject + ":last-child");
+            newTaskAnimation?.classList.add(style.newTaskAnimation);
+        }, 1);
+    };
 
-    render() {
-        return (
-            <div className={style.App}>
-                <div className={style.todoMainDiv}>
-                    <form className={style.addTodoForm} onSubmit={this.handleSubmit}>
-                        <label htmlFor='todoName'>Todo : </label>
-                        <input className={style.addTodoInput} placeholder="Add your task" name="todoName" type="text" onChange={this.handleChange} />
-                        <input className={style.addTodoBtn} value={'Ajouter'} type="submit" />
-                    </form>
-                </div>
-                <div className={style.todoMainObject}>
-                    {this.state.todoList.map((todoName: string, index: number) => {
-                        return (
-                            <div key={todoName} className={style.todoObject}>
-                                <button onClick={(e) => this.deleteTodoElement(index,e)}>X</button>
-                                <p>{todoName}</p>
-                            </div>
-                        )
-                    })}
-                </div>
+    return (
+        <div className={style.App}>
+            <TodoListForm handleSubmit={handleSubmit} handleChange={handleChange} />
+            <div className={style.todoMainObject}>
+                {todoList.map((todoName: string, index: number) => {
+                    return (
+                        <div key={todoName + index} className={style.todoObject}>
+                            <button onClick={(e) => deleteTodoElement(index, e)}>X</button>
+                            <p>{todoName}</p>
+                        </div>
+                    );
+                })}
             </div>
-        )
-    }
+        </div>
+    );
 }
+
+export default TodoListCss;
